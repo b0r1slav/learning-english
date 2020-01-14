d959d5cce9f9f867a6e08d96ee57785c1092860a
b0r1slav <borislav.madzharov@gmail.com>
2020-01-14 21:43:38 +0200

    Updates

---
+++ precache-manifest.7c4e4a0925cebbf1cbcf38d7249cd6af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e244f751fc0e752eae1061f74ff42436",
+    "url": "/learning-english/index.html"
+  },
+  {
+    "revision": "13bef0f5fe17f5ba982c",
+    "url": "/learning-english/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "59d57d36b8c64105517a",
+    "url": "/learning-english/static/css/main.1c2fe9ad.chunk.css"
+  },
+  {
+    "revision": "13bef0f5fe17f5ba982c",
+    "url": "/learning-english/static/js/2.54b1d5aa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learning-english/static/js/2.54b1d5aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59d57d36b8c64105517a",
+    "url": "/learning-english/static/js/main.4ca6f402.chunk.js"
+  },
+  {
+    "revision": "234504d03f3aeaabf29b",
+    "url": "/learning-english/static/js/runtime-main.c8562280.js"
+  }
+]);
